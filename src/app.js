ed2485da305a93b8daf7ce3de18f38d2f244de03
osicrat <8xnecrox8@gmail.com>
2022-11-29 12:37:43 -0300

    Projeto das aulas de node-balta.io

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const config = require('./config')
+const app = express();
+const mongoose = require('mongoose')
+const router = express.Router();
+
+mongoose.connect(config.connectionString);
+
+app.use(bodyParser.json(
+    { limit: '5mb' }
+));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Alloe-Origin', '*');
+    res.header('Access-Control-Alloe-Origin', 'Origin, X-Requested-with, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Alloe-Origin', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
+
+const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
+
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
+const orderRoute = require('./routes/order-routes');
+
+
+
+
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+app.use('/customers', customerRoute);
+app.use('/orders', orderRoute);
+
+
+
+
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+    connectionString: 'mongodb+srv://junior:Y5ml5Z4ew6DlECwU@cluster0.usazu.mongodb.net/junior?retryWrites=true&w=majority',
+    sendgridKey: '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',
+    containerConnectionString: ''
+}
